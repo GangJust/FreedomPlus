9a3bc29556cc70aa0bda9fb154cf91e9e3344339
Gang <freegang555@gmail.com>
2024-07-01 21:19:17 +0800

    optimize

--- core/src/main/java/io/github/fplus/core/hook/HPhoneWindow.kt
+++ core/src/main/java/io/github/fplus/core/hook/HPhoneWindow.kt
@@ -1,8 +1,11 @@
 package io.github.fplus.core.hook
 
+import android.content.Context
 import android.graphics.Color
+import com.freegang.extension.findMethodInvoke
 import de.robv.android.xposed.XC_MethodHook
 import io.github.fplus.core.config.ConfigV1
+import io.github.fplus.core.ui.activity.FreedomSettingActivity
 import io.github.xpler.core.entity.HookEntity
 import io.github.xpler.core.entity.OnBefore
 import io.github.xpler.core.hookBlockRunning
@@ -19,9 +22,16 @@ class HPhoneWindow : HookEntity() {
         return findClass("com.android.internal.policy.PhoneWindow")
     }
 
+    private fun getContext(params: XC_MethodHook.MethodHookParam): Context? {
+        return params.thisObject.findMethodInvoke<Context> { name("getContext") }
+    }
+
     @OnBefore("setStatusBarColor")
     fun setStatusBarColorBefore(params: XC_MethodHook.MethodHookParam, color: Int) {
         hookBlockRunning(params) {
+            if (getContext(this) is FreedomSettingActivity)
+                return
+
             if (config.isImmersive) {
                 args[0] = Color.TRANSPARENT
             }
@@ -33,6 +43,9 @@ class HPhoneWindow : HookEntity() {
     @OnBefore("setNavigationBarColor")
     fun setNavigationBarColorBefore(params: XC_MethodHook.MethodHookParam, color: Int) {
         hookBlockRunning(params) {
+            if (getContext(this) is FreedomSettingActivity)
+                return
+
             if (config.isImmersive) {
                 args[0] = Color.TRANSPARENT
             }
